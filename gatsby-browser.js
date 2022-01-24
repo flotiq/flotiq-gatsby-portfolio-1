0389a3d981f764354dfee800de81c6119319f0ce
Bartek Musiał <bartlomiej.musial@hotmail.com>
2022-01-25 00:16:39 +0100

    portfolio project description component

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,3 +1,4 @@
 import './src/style/global.css';
 import '@fontsource/playfair-display/400-italic.css';
 import '@fontsource/playfair-display/700.css';
+import '@fontsource/sora/300.css';
--- src/templates/portfolio-project.js
+++ src/templates/portfolio-project.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import { graphql } from 'gatsby';
 import Layout from '../layouts/layout';
+import ProjectDescription from '../components/portfolio/ProjectDescription';
 
 const PortfolioProjectTemplate = ({ data }) => {
     const { project } = data;
 
     return (
         <Layout additionalClass={['bg-white']}>
-            <div>1</div>
+            <div className="flex flex-wrap max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
+                <div className="flex basis-full md:basis-1/2" />
+                <div className="flex basis-full md:basis-1/2 pl-0 md:pl-12 py-5">
+                    <ProjectDescription description={project.description} additionalClass={['text-base md:text-lg']} />
+                </div>
+            </div>
         </Layout>
     );
 };
