9a93f777c19a16c37d6c07fee4937c32210f3938
Bartek Musiał <bartlomiej.musial@hotmail.com>
2022-01-28 15:25:25 +0100

    project gallery build

--- src/components/project/ProjectGallery.js
+++ src/components/project/ProjectGallery.js
@@ -1,5 +1,6 @@
 import React, { useRef } from 'react';
 import ImageGallery from 'react-image-gallery';
+import { ArrowLeftIcon, ArrowRightIcon } from '@heroicons/react/solid';
 
 const ProjectGallery = ({ gallery, galleryName, galleryDescription, name }) => {
     const images = gallery.map((image) => ({
@@ -9,17 +10,34 @@ const ProjectGallery = ({ gallery, galleryName, galleryDescription, name }) => {
     const LeftNav = React.memo(({ disabled, onClick }) => (
         <button
             type="button"
-            className="image-gallery-icon image-gallery-left-nav"
+            className="flex items-center justify-start !p-0 image-gallery-icon image-gallery-left-nav left-0
+            !right-auto -bottom-14 !top-auto underline !text-primary font-sora !drop-shadow-none !transform-none
+            text-sm md:text-base"
             disabled={disabled}
             onClick={onClick}
             aria-label="Previous Slide"
         >
-            left
+            <ArrowLeftIcon className="mr-3 h-5 w-5 text-primary" />
+            Previous work
+        </button>
+    ));
+    const RightNav = React.memo(({ disabled, onClick }) => (
+        <button
+            type="button"
+            className="flex items-center justify-end !p-0 image-gallery-icon image-gallery-left-nav right-0
+            !left-auto -bottom-14 !top-auto underline !text-primary font-sora !drop-shadow-none !transform-none
+            text-sm md:text-base"
+            disabled={disabled}
+            onClick={onClick}
+            aria-label="Next Slide"
+        >
+            Next work
+            <ArrowRightIcon className="ml-3 h-5 w-5 text-primary" />
         </button>
     ));
 
     return (
-        <div className="flex flex-wrap items-center max-w-7xl mx-auto px-2 sm:px-6 lg:px-8 py-10">
+        <div className="flex flex-wrap items-center max-w-7xl mx-auto px-2 sm:px-6 lg:px-8 pt-10 pb-24">
             <div className="basis-full md:basis-1/4 pr-5 mb-6 md:mb-0">
                 <h2 className="font-semibold text-2xl mb-5" dangerouslySetInnerHTML={{ __html: galleryName }} />
                 <p className="font-sora font-lg" dangerouslySetInnerHTML={{ __html: galleryDescription }} />
@@ -35,6 +53,14 @@ const ProjectGallery = ({ gallery, galleryName, galleryDescription, name }) => {
                             disabled={imageGallery.current?.getCurrentIndex() === 1}
                             onClick={() => imageGallery.current?.slideToIndex(
                                 imageGallery.current.getCurrentIndex() - 1,
+                            )}
+                        />
+                    )}
+                    renderRightNav={() => (
+                        <RightNav
+                            disabled={imageGallery.current?.getCurrentIndex() === -1}
+                            onClick={() => imageGallery.current?.slideToIndex(
+                                imageGallery.current.getCurrentIndex() + 1,
                             )}
                         />
                     )}
